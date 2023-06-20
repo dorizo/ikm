ac76201b3cf4145d44d5b2f7b1ae979ec6c20466
doris hermawan <dorizo89@gmail.com>
2023-06-20 10:22:42 +0700

    iuran

--- application/views/frondend/iuran.php
+++ application/views/frondend/iuran.php
@@ -4,8 +4,30 @@
       <div class="page-content">
 
         <div class="card rounded-3 border-0 bg-transparent">
-          
-        </div>
-      </div>
+    <div class="card-body p-0">
+        <form>
+            <div class="row row-cols-1 g-3">
+                <div class="col">
+                    <div class="form-floating">
+                    <input type="number" class="form-control rounded-3" id="floatingInputName" placeholder="Name" value="jhon Deo">
+                    <label for="floatingInputName">JUMLAH IURAN</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-floating">
+                    <input type="date" class="form-control rounded-3" id="floatingInputName" placeholder="Name" >
+                    <label for="floatingInputName">TANGGAL IURAN</label>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-floating">
+                    <input type="file" class="form-control rounded-3">
+                    <label for="floatingInputNumber">Foto Pembayaran</label>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+ </div>
 <!--end to page content-->
 <?php include("menubawah.php")?>
