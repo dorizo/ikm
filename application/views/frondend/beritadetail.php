65335aa0f3133a400e866971a3287612cab63210
doris hermawan <dorizo89@gmail.com>
2023-08-06 12:22:03 +0700

    ss

--- application/views/frondend/beritadetail.php
+++ application/views/frondend/beritadetail.php
@@ -6,7 +6,7 @@
   $EEEE= $this->db->query("select * from berita where id_berita=$idberita   order by id_berita desc")->result_array();
   foreach ($EEEE as $key => $value) {
   ?>
-    <div class="card">
+    <div class="card m-2 pt-2">
         <div class="card-body">
           <h6 class="fw-bold mb-1 text-dark"><?=$value["judul_berita"]?></h6>
          <img src="<?=base_url("admin/assets/upload/image/thumbs")."/".$value["gambar"]?>" class="rounded-3" width="100%" alt=""> <br />
